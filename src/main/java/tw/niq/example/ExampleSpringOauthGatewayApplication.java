88d2425b17e9d587a2e8c37ac02204c9b7374b39
Nick <nick.chen@cmp.com.tw>
2023-07-26 16:51:10 +0800

    Initial commit

---
+++ src/main/java/tw/niq/example/ExampleSpringOauthGatewayApplication.java
@@ -0,0 +1,13 @@
+package tw.niq.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringOauthGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringOauthGatewayApplication.class, args);
+	}
+
+}
